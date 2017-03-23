4d28599ec1abd037a1404ead58cf52b168544488
Kristel <kristel_pire@hotmail.com>
2017-03-23 12:53:45 +0100

    added folder classes & updated login.php

--- login.php
+++ login.php
@@ -1,10 +1,22 @@
-<!DOCTYPE html>
+<?php
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
 <body>
-    
+
+    <form action="" method="post">
+        <label for="email"></label>
+        <input name="email" type="email">
+
+        <label for="password"></label>
+        <input name="password" type="password">
+
+        <button type="submit">LOGIN</button>
+    </form>
+
 </body>
 </html>
