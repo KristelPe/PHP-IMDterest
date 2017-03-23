32aa82402ba8564cf9de184c149a4bf5a20bc1ab
Kristel <kristel_pire@hotmail.com>
2017-03-23 15:00:30 +0100

    updated login.php

--- login.php
+++ login.php
@@ -1,5 +1,29 @@
 <?php
 
+    if( !empty($_POST) ){
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $conn = new PDO('mysql:host=localhost;dbname=IMDterest', 'root', '');
+        $statement = $conn->prepare("SELECT * FROM users WHERE email = :email ;");
+        $statement->bindValue(":email", $email);
+        $res = $statement->execute();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+        foreach( $results as $row ){
+            if(password_verify($password, $row['password'])){
+                header('Location: login.php');
+            }
+            else
+            {
+                session_start();
+                $_SESSION['email'] = $email;
+                header('Location: index.php');
+            }
+        }
+    }
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,14 +33,13 @@
 <body>
 
     <form action="" method="post">
-        <label for="email"></label>
-        <input name="email" type="email">
+        <label for="email">Email</label>
+        <input id="email" name="email" type="email">
 
-        <label for="password"></label>
-        <input name="password" type="password">
+        <label for="password">Password</label>
+        <input id="password" name="password" type="password">
 
         <button type="submit">LOGIN</button>
     </form>
-
 </body>
 </html>
