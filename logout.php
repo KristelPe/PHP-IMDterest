6fcec1d224a0eb3e13c34171ad757ad44fb88ef0
sorenwagemans <soren.wagemans@hotmail.com>
2017-03-23 12:43:34 +0100

    Logout page done

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+session_destroy();
+header('Location: login.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Logout</title>
+</head>
+<body>
+
+</body>
+</html>
