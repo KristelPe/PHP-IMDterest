4410a2d5d41a31144c32b67270457f380d57fcde
Kristel <kristel_pire@hotmail.com>
2017-03-24 12:30:58 +0100

    added classe Db

---
+++ classes/Db.class.php
@@ -0,0 +1,18 @@
+<?php
+
+abstract class Db
+{
+    private static $conn = NULL;
+
+    public static function getInstance()
+    {
+        if(isset($conn)){
+            //er is een connectie geef ze terug
+            return self::$conn;
+        } else{
+            //er is nog geen connectie maak ze aan en geef ze terug
+            self::$conn = new PDO("mysql:host=localhost; dbname=IMDterest","root", "");
+            return self::$conn;
+        }
+    }
+}
