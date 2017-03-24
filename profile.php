342318586a4a224a0710a7c644cedd7fef1cfc07
Robbe Reygel <Robbe Reygel>
2017-03-24 17:17:13 +0100

    Finished ability to change username in profile.php

--- profile.php
+++ profile.php
@@ -7,6 +7,33 @@
         header('location: login.php');
     }
 
+    //get email address of current user/session
+    $email = $_SESSION['user'];
+
+    //find username associated with the email address
+    $connection = new PDO('mysql:host=localhost; dbname=IMDterest', 'root', '');
+    $statement = $connection->prepare("SELECT username FROM users WHERE email = :email");
+    $statement->bindvalue(":email", $email);
+    $res = $statement->execute();
+    $username = $statement->fetchColumn();
+
+    //Replace old username by new username
+    try{
+        if(!empty ($_POST['username']) && ($_POST['username']) != $username){
+            $newUsername = $_POST['username'];
+            $connection = new PDO('mysql:host=localhost; dbname=IMDterest', 'root', '');
+            $statement = $connection->prepare("UPDATE users SET username = REPLACE(username, '$username', '$newUsername') WHERE INSTR(username, '$username') > 0;");
+            $res = $statement->execute();
+            $usernameSuccess = "Your username has been changed to " . "<b>" . $newUsername . "</b>";
+        }else{
+            $usernameError = "Please fill in a valid username!";
+        }
+    }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -40,6 +67,8 @@
         </button>
         <br>
         <br>
+        <p><?php echo "Your current username is " . "<b>" . $username . "</b>" ?></p>
+        <p><?php if(isset($usernameError)){echo $usernameError;}else if(isset($usernameSuccess)){echo $usernameSuccess;} ?></p>
         <label for="name">Change username</label>
         <input type="text" name="username" id="username" placeholder="New username">
         <button>
