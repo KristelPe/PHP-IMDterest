1e54bc3c6aecefff022762818147c07c3d6a6253
MichielJanssens <michiel.janssens88@gmail.com>
2017-04-13 16:49:46 +0200

    Spark brand 2
    added splash page

---
+++ intro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/background.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+    <div id="intro">
+        <img src="images/spark_logo.svg" alt="logo">
+        <h1>Spark</h1>
+        <h2>For IMD</h2>
+        <nav>
+            <a href="login.php">Click here to login</a>
+            <a href="registration.php">Click here to register</a>
+        </nav>
+    </div>
+</body>
+</html>
