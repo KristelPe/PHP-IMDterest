0747a43eea04d41e8fad33a2e648608fde4a1c5b
Kristel <kristel_pire@hotmail.com>
2017-04-14 00:31:34 +0200

    added load more button and items

--- index.php
+++ index.php
@@ -6,18 +6,34 @@
         header('location: login.php');
     }
 
-
-    if(isset($_POST['search'])) {
-        $searchq = $_POST['search'];
-        $searchq = preg_replace("#[^0-9a-z]#i", "", $searchq);
-
+    try{
         $connection = new PDO('mysql:host=localhost; dbname=IMDterest', 'root', '');
-        $statement = $connection->prepare("SELECT * FROM posts WHERE title LIKE :keywords OR description LIKE :keywords");
-        $statement->bindValue(':keywords', '%' . $searchq . '%');
-        $statement->execute();
-        while ($r = $statement->fetch(PDO::FETCH_ASSOC)) {
-            echo "<pre>" . print_r($r, true) . "</pre>";
+
+        if(isset($_POST['search'])) {
+            $searchq = $_POST['search'];
+            $searchq = preg_replace("#[^0-9a-z]#i", "", $searchq);
+
+            $statement = $connection->prepare("SELECT * FROM posts WHERE title LIKE :keywords OR description LIKE :keywords");
+            $statement->bindValue(':keywords', '%' . $searchq . '%');
+            $statement->execute();
+            /*while ($r = $statement->fetch(PDO::FETCH_ASSOC)) {
+                echo "<pre>" . print_r($r, true) . "</pre>";
+            }*/
+        } else {
+            $statement = $connection->prepare("select * from posts order by id DESC ");
+            $statement->execute();
         }
+
+    }catch(Exception $e) {
+        echo $e->getMessage();
+    }
+
+    $i = 0;
+
+    $count = 22;
+    if(isset($_POST['more']))
+    {
+        $count = $count + 20;
     }
 
 ?>
@@ -25,23 +41,79 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>IMDterest</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/search.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <style>
+
+        .post_img{
+            width: 230px;
+            border-radius: 5px;
+            max-height: 200px;
+            overflow: hidden;
+        }
+
+        img{
+            width: 230px;
+        }
+
+        .like{
+            display: flex;
+            flex-direction: row;
+            margin-top: 1em;
+        }
+
+        p{
+            margin-top: 0.5em;
+            color: #9da5b6;
+        }
+
+        .like{
+            line-height: 1em;
+        }
+
+        .like p, .user p{
+            color: #b4b4b4;
+            font-size: small;
+        }
+
+        .like button{
+            margin: 0;
+            margin-right: 1em;
+            width: 30px;
+            height: 30px;
+            padding:0;
+            background-position: center;
+            background-size: 20px;
+            background-repeat: no-repeat;
+            background-color: white;
+            border-radius: 5px;
+        }
+
+        .liked{
+            background-image: url("http://www.clker.com/cliparts/o/4/f/P/U/b/red-heart-hi.png");
+        }
+
+        .unliked{
+            background-image: url("http://www.clker.com/cliparts/d/v/C/j/y/1/grey-heart-hi.png");
+        }
+
+        .item{
+            max-height: 500px;
+        }
+
+    </style>
 </head>
 <body>
-    <div id="navigatie">
-        <img src="images/spark_logo.svg" alt="spark_logo">
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="upload.php">Upload</a>
-            <a href="profile.php">Profile</a>
-            <a href="logout.php">Logout</a>
-        </nav>
-    </div>
+   <nav>
+    <a href="index.php">Home</a>
+    <a href="upload.php">Upload</a>
+    <a href="profile.php">Profile</a>
+    <a href="logout.php">Logout</a>
+    </nav>
 
    <div id="search_bar_style">
        <form action="index.php" method="post" id="search_bar">
@@ -54,37 +126,32 @@
 
        <div class="item_layout">
 
+           <?php foreach ($statement as $p): if (++$i == $count) break; ?>
            <div class="item">
-               <div class="item_img">
-               </div>
-           </div>
+               <h1><?php echo $p['title']?></h1>
+               <div class="post_img">
+                   <img src="<?php echo $p['image']?>" alt="<?php echo $p['title']?>">
 
-           <div class="item">
-               <div class="item_img">
                </div>
-           </div>
-
-           <div class="item">
-               <div class="item_img">
+               <div class="like">
+                   <button class="unliked"></button>
+                   <p>[#likes]</p>
                </div>
+               <p><?php  echo $p['description']?></p>
            </div>
+           <?php endforeach;?>
 
-           <div class="item">
-               <div class="item_img">
-               </div>
-           </div>
 
-           <div class="item">
-               <div class="item_img">
-               </div>
-           </div>
-
-           <div class="item">
-               <div class="item_img">
-               </div>
-           </div>
-
+           <!--
+           TO DO:
+            - like/unlike: toggle class use AJAX
+            - only show content from friends
+           -->
        </div>
+
+       <form method="POST" action=''>
+        <?php if(!isset($_POST['search'])) { echo "<button type='submit' name='more' id='more'>Load more</button>"; } ?>
+       </form>
    </div>
 </body>
 </html>
