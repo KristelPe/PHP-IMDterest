9378c4d5caa3c5a09771c551e544b9aa79a0d131
MichielJanssens <michiel.janssens88@gmail.com>
2017-04-16 16:10:46 +0200

    Startfiles post.php

---
+++ post.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/post.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+<div id="navigatie">
+    <img src="images/spark_logo.svg" alt="spark_logo">
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="upload.php">Upload</a>
+        <a href="profile.php">Profile</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+</div>
+
+<div id="container">
+    <div id="post_layout">
+        <img src="" alt="foto_post">
+        <div>
+            <h1>Naam post</h1>
+            <h2>Publisher</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis volutpat, nisl tristique gravida euismod, elit ipsum pulvinar urna, a congue quam sapien at mi. Suspendisse tortor arcu, cursus sed nisi ut, commodo consequat felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent eget iaculis neque, a semper diam. Donec turpis purus, elementum dignissim odio vitae, pharetra vulputate felis. Suspendisse potenti. Phasellus pellentesque hendrerit malesuada. Proin interdum in tortor ut laoreet. Morbi at rutrum justo. Donec arcu lacus, facilisis suscipit mi non, vulputate vehicula nunc. Phasellus ullamcorper, ex commodo tristique consectetur, mi tortor porta ex, sed sodales purus ex sit amet nisi. Pellentesque id ligula consectetur, vestibulum nunc non, sagittis ex. Vivamus aliquet vulputate libero, eget euismod libero sodales a.
+
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
