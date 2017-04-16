c4188c693447ff8a56dd52be42ec01b0111cf1ab
Kristel <kristel_pire@hotmail.com>
2017-04-17 00:20:16 +0200

    added load more button using ajax

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
             $searchq = $_POST['search'];
             $searchq = preg_replace("#[^0-9a-z]#i", "", $searchq);
 
-            $statement = $connection->prepare("SELECT * FROM posts WHERE title LIKE :keywords OR description LIKE :keywords");
+            $statement = $connection->prepare("SELECT * FROM posts WHERE title LIKE :keywords OR description LIKE :keywords limit 0,20");
             $statement->bindValue(':keywords', '%' . $searchq . '%');
             $statement->execute();
             /*while ($r = $statement->fetch(PDO::FETCH_ASSOC)) {
                 echo "<pre>" . print_r($r, true) . "</pre>";
             }*/
         } else {
-            $statement = $connection->prepare("select * from posts order by id DESC ");
+            $statement = $connection->prepare("select * from posts order by id DESC limit 0,20");
             $statement->execute();
         }
 
@@ -30,12 +30,6 @@
     }
 
     $i = 0;
-
-    $count = 22;
-    if(isset($_POST['more']))
-    {
-        $count = $count + 20;
-    }
 
 ?>
 <!DOCTYPE html>
@@ -48,82 +42,144 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/search.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <style>
+        .post_img{
+            width: 230px;
+            border-radius: 5px;
+            max-height: 200px;
+            overflow: hidden;
+        }
+        img{
+            width: 230px;
+        }
+        .like{
+            display: flex;
+            flex-direction: row;
+            margin-top: 1em;
+        }
+        p{
+            margin-top: 0.5em;
+            color: #9da5b6;
+        }
+        .like{
+            line-height: 1em;
+        }
+        .like p, .user p{
+            color: #b4b4b4;
+            font-size: small;
+        }
+        .like button{
+            margin: 0;
+            margin-right: 1em;
+            width: 30px;
+            height: 30px;
+            padding:0;
+            background-position: center;
+            background-size: 20px;
+            background-repeat: no-repeat;
+            background-color: white;
+            border-radius: 5px;
+        }
+        .liked{
+            background-image: url("http://www.clker.com/cliparts/o/4/f/P/U/b/red-heart-hi.png");
+        }
+        .unliked{
+            background-image: url("http://www.clker.com/cliparts/d/v/C/j/y/1/grey-heart-hi.png");
+        }
+        .item{
+            max-height: 500px;
+        }
+    </style>
 </head>
 <body>
-<div id="navigatie">
-    <img src="images/spark_logo.svg" alt="spark_logo">
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="upload.php">Upload</a>
-        <a href="profile.php">Profile</a>
-        <a href="logout.php">Logout</a>
-    </nav>
+<nav>
+    <a href="index.php">Home</a>
+    <a href="upload.php">Upload</a>
+    <a href="profile.php">Profile</a>
+    <a href="logout.php">Logout</a>
+</nav>
+
+<div id="search_bar_style">
+    <form action="index.php" method="post" id="search_bar">
+        <input type="text" name="search" id="search" placeholder="Search..." />
+        <button type="submit" id="search_bar_button"></button>
+    </form>
 </div>
-   <div id="search_bar_style">
-       <form action="index.php" method="post" id="search_bar">
-           <input type="text" name="search" id="search" placeholder="Search..." />
-           <button type="submit" id="search_bar_button"></button>
-       </form>
-   </div>
 
-   <div id="container">
+<div id="container">
 
-       <div class="item_layout">
+    <div id="items" class="item_layout">
 
-           <?php foreach ($statement as $key => $p): if (++$i == $count) break; ?>
-           <div class="item">
-               <h1><?php echo $p['title']?></h1>
+        <?php $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-               <?php if(!empty($p['link'])) : ?>
+        foreach( $results as $key => $p ){
+            if(!empty($p['link'])){
+                $html = file_get_html($p['link']);
+                $pagetitle = $html->find('title', 0);
+                $image = $html->find('img', 0);
 
-               <a href="<?php echo $p['link']?>">
-                   <?php
-                       $html = file_get_html($p['link']);
-                       $pagetitle = $html->find('title', 0);
-                       $image = $html->find('img', 0);
-
-                       echo $pagetitle->plaintext;
-                   ?>
-               </a>
-                   <a href="post.php?postid=<?php echo $key;?>">
-                       <div class="post_img">
-                       <img src=
-                            "<?php
-                                echo $image->src;
-                            ?>"
-                            alt=
-                            "<?php
-                                echo $pagetitle->plaintext;
-                            ?>"
-                       >
-                   </div>
-               </a>
-
-
-               <?php endif; ?>
-               <?php if(empty($p['link'])) : ?>
-                   <a href="post.php?postid=<?php echo $key;?>">
-                   <div class="post_img">
-                       <img src="<?php echo $p['image']?>" alt="<?php echo $p['title']?>">
-
-                   </div>
+                echo "<div id='item' class='item'>
+                   <a href='" . $p['link'] . "'>" .
+                    $pagetitle->plaintext
+                    . "</a>
+                       <a href='post.php?postid='" . $p['id'] . "'>
+                           <div class='post_img'>
+                           <img src='
+                                " .
+                    $image->src
+                    . "'
+                                alt='
+                                " .
+                    $pagetitle->plaintext
+                    . "'
+                           >
+                       </div>
                    </a>
-                   <?php endif; ?>
-                   <div class="like">
-                       <button class="unliked"></button>
-                       <p>[#likes]</p>
-                   </div>
-                   <p><?php  echo $p['description']?></p>
+                   </div>";
+            } elseif(empty($p['link'])) {
+                echo "<div id='item' class='item'>
+                       <h1>" . $p['title'] . "</h1>
+                       <a href='post.php?postid=" . $p['id'] . "'>
+                           <div class='post_img'>
+                               <img src='" . $p['image'] . "' alt='" . $p['title'] . "'>
+                           </div>
+                       </a>
+                       <div class='like'>
+                           <button class='unliked'></button>
+                           <p>[#likes]</p>
+                       </div>
+                       <p>" . $p['description'] . "</p>
+                   </div>";
+            }
+        }
+        ?>
+        <input type="hidden" id="result_no" value="20">
+    </div>
+    <button type='submit' name='more' id='more'>Load more</button>
+</div>
 
-           </div>
-           <?php endforeach;?>
+<script src="jquery.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#more").click(function(){
+            loadmore();
+        });
+    });
 
-       </div>
-
-
-       <form method="POST" action='' id="form_more">
-        <?php if(!isset($_POST['search'])) { echo "<button type='submit' name='more' id='more'>Load more</button>"; } ?>
-       </form>
-   </div>
+    function loadmore() {
+        var val = document.getElementById("result_no").value;
+        $.ajax({
+            type: 'post',
+            url: 'ajax/loadMore.php',
+            data: {     getresult:val   },
+            success: function (response) {
+                var content = document.getElementById("items");
+                content.innerHTML = content.innerHTML+response;
+                // LIMIT + 20
+                document.getElementById("result_no").value = Number(val)+20;
+            }
+        });
+    }
+</script>
 </body>
 </html>
