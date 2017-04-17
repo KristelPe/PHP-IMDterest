7dc12c16bf7afcfc26cffade63ddb4017f0e7c9f
MichielJanssens <michiel.janssens88@gmail.com>
2017-04-17 19:38:46 +0200

    fixed index layout

--- index.php
+++ index.php
@@ -92,12 +92,15 @@
     </style>
 </head>
 <body>
-<nav>
-    <a href="index.php">Home</a>
-    <a href="upload.php">Upload</a>
-    <a href="profile.php">Profile</a>
-    <a href="logout.php">Logout</a>
-</nav>
+<div id="navigatie">
+    <img src="images/spark_logo.svg" alt="spark_logo">
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="upload.php">Upload</a>
+        <a href="profile.php">Profile</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+</div>
 
 <div id="search_bar_style">
     <form action="index.php" method="post" id="search_bar">
