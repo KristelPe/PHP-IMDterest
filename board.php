67fae069af1fb1e073bdb49d688366082e8879d4
BuildTools <robbereygel97@gmail.com>
2017-04-23 19:10:29 +0200

    added board.php starter file

---
+++ board.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Make a board</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <style>
+
+    </style>
+</head>
+<body>
+<?php include_once("nav.inc.php")?>
+</body>
+</html>
+
