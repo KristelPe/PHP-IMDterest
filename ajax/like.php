93f87144086798e1fac8cc15467c48ecb5c697f6
Kristel <kristel_pire@hotmail.com>
2017-04-27 23:06:48 +0200

    updated ajax like - still got one tiny problem

--- ajax/like.php
+++ ajax/like.php
@@ -2,30 +2,30 @@
     include_once "../classes/Db.class.php";
 
     session_start();
+    $postId = 2;                    //DUUUUUUS dees zou normaal $_POST['postId'] moeten zijn ma da werkt (Undefined index) -> kan waarde uit jquery niet ophalen dus k denk da de fout in dn jquery zit ╮(─▽─)╭
+    settype($postId, "integer");
 
-    $postId = $_POST['id'];
     $count = $_POST['count'];
     $userId = $_SESSION['id'];
 
     $pdo = Db::getInstance();
 
-    if ($count == true) {
+    if ($count == 'plus') {
         $stmt = $pdo->prepare("INSERT INTO likes (user_id, post_id) VALUES (:userid, :postid)");
         $stmt->bindValue("userid", $userId);
         $stmt->bindValue("postid", $postId);
         $stmt->execute();
-    } elseif ($count == false) {
-        $stmt = $pdo->prepare("DELETE FROM likes WHERE postid = :postid AND userid = :userid");
+    } elseif ($count == 'minus') {
+        $stmt = $pdo->prepare("DELETE FROM likes WHERE post_id = :postid AND user_id = :userid");
         $stmt->bindValue("userid", $userId);
         $stmt->bindValue("postid", $postId);
         $stmt->execute();
     }
 
-    $statement = $pdo->prepare("SELECT count(*) FROM likes WHERE postid = :postid");
+    $statement = $pdo->prepare("SELECT count(*) FROM likes WHERE post_id = :postid");
     $statement->bindValue("postid", $postId);
-    $likes = $statement->execute();
+    $statement->execute();
 
-    echo $likes;
-
+    echo $statement->execute();
 
     // PROBLEEM 3 - nog niet kunnen testen door probleem 1  ╮(─▽─)╭
