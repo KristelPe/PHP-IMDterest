70b50007fe85cc73caf7901a0c02e13a5b7d9f38
Kristel <kristel_pire@hotmail.com>
2017-05-04 09:51:05 +0200

    updated Post - added functions like, unlike and countLikes

--- classes/Post.class.php
+++ classes/Post.class.php
@@ -253,4 +253,29 @@ class Post
         return $results;
     }
 
+    public function Like(){
+        $conn = Db::getInstance();
+        $stmt = $conn->prepare("INSERT INTO likes (user_id, post_id) VALUES (:userid, :postid)");
+        $stmt->bindValue("userid", $this->m_userId);
+        $stmt->bindValue("postid", $this->m_postId);
+        $stmt->execute();
+    }
+
+    public function Unlike(){
+        $conn = Db::getInstance();
+        $stmt = $conn->prepare("DELETE FROM likes WHERE post_id = :postid AND user_id = :userid");
+        $stmt->bindValue("userid", $this->m_userId);
+        $stmt->bindValue("postid", $this->m_postId);
+        $stmt->execute();
+    }
+
+    public function CountLikes(){
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("SELECT count(*) as likes FROM likes WHERE post_id = :postid");
+        $statement->bindValue("postid", $this->m_postId);
+        $statement->execute();
+        $res = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
+    }
+
 }
