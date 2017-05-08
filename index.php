a90ba915d06bcfb59006dbd3cb2b611bd59a939a
Kristel <kristel_pire@hotmail.com>
2017-05-08 22:30:08 +0200

    adjusted load more

--- index.php
+++ index.php
@@ -191,10 +191,14 @@
             url: 'ajax/loadMore.php',
             data: {     getresult:val   },
             success: function (response) {
-                var content = document.getElementById("items");
-                content.innerHTML = content.innerHTML+response;
-                // LIMIT + 20
-                document.getElementById("result_no").value = Number(val)+20;
+                if (response == "empty"){
+                    $("#more").text("There are no more posts")
+                } else {
+                    var content = document.getElementById("items");
+                    content.innerHTML = content.innerHTML + response;
+                    // LIMIT + 20
+                    document.getElementById("result_no").value = Number(val) + 20;
+                }
             }
         });
     }
