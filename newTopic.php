0f30012a8d118abdee62c39a583f193e48a38e5e
Kristel <kristel_pire@hotmail.com>
2017-05-09 22:47:44 +0200

    updated newTopic - working + errormsg

--- newTopic.php
+++ newTopic.php
@@ -11,22 +11,57 @@
 
 try{
     $error = "";
+    $succes = "";
     if(!empty($_POST))
     {
-        $target_dir = "uploads/";
         $title = htmlspecialchars($_POST['title']);
-        $target_file = $target_dir . basename($_FILES["image"]["name"]);
-        if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
+        if (!empty($_FILES["image"]["name"])) {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["image"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+            // Check if image file is a actual image or fake image
+            if (isset($_POST["submit"])) {
+                $check = getimagesize($_FILES["image"]["tmp_name"]);
+                if ($check !== false) {
+                    $succes = "File is an image - " . $check["mime"] . ". ";
+                    $uploadOk = 1;
+                } else {
+                    $error = "File is not an image. ";
+                    $uploadOk = 0;
+                }
+            }
+            // Check file size
+            if ($_FILES["image"]["size"] > 500000) {
+                $error = $error . "Sorry, your file is too large. ";
+                $uploadOk = 0;
+            }
+            // Allow certain file formats
+            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif") {
+                $error = $error . "Sorry, only JPG, JPEG, PNG & GIF files are allowed. ";
+                $uploadOk = 0;
+            }
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                $error = $error . "Sorry, your file was not uploaded. ";
+                // if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                    echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+                } else {
+                    echo "Sorry, there was an error uploading your file.";
+                }
+            }
         }
 
-        if(!empty($target_file)) {
+        if(!empty($target_file) && !empty($title)) {
             $newT = new topic();
             $newT->setTitle($title);
             $newT->setImg($target_file);
             $newT->newT();
+        }else {
+            $error = "Please fill in all the fields.";
         }
     }
 }
@@ -91,12 +126,17 @@ catch(Exception $e) {
             background-color: #919191;
             color: white;
         }
+        .alert{
+            color: crimson;
+        }
     </style>
 </head>
 <body>
 <?php include_once("nav.inc.php")?>
 
 <form id="new" action="" method="post" enctype="multipart/form-data">
+    <?php echo "<h3 class='alert'>".$error. $succes ."</h3></br>" ?>
+
     <h2>Make a new topic</h2>
 
     <div>
