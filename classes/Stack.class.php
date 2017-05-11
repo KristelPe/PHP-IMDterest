e4765da4460b857cacee875d785901f435988abb
MichielJanssens <michiel.janssens88@gmail.com>
2017-05-11 16:57:32 +0200

    reported query (only finished " if  followers = 0 ")

--- classes/Stack.class.php
+++ classes/Stack.class.php
@@ -61,12 +61,26 @@ class Stack
         $status =  $stmnt->fetchAll(PDO::FETCH_ASSOC);
 
         if (!empty($status)) {
-            $statement = $conn->prepare("select p.*, count(l.user_id) as likes, u.username as username, u.image as userImage from posts p inner join following f on f.userid = p.userid inner join users u on p.userId = u.id left join likes l on p.id = l.post_id where followerid = :follwerid group by p.id ORDER BY p.id desc limit :noP,20");
+            $statement = $conn->prepare("select p.*, count(l.user_id) as likes, u.username as username, u.image as userImage 
+                                        from posts p 
+                                        inner join following f on f.userid = p.userid 
+                                        inner join users u on p.userId = u.id 
+                                        left join likes l on p.id = l.post_id 
+                                        where followerid = :follwerid 
+                                        group by p.id 
+                                        ORDER BY p.id desc limit :noP,20");
             $statement->bindValue(':followerid', $this->sessionId);
             $statement->bindValue(':noP', $noP, PDO::PARAM_INT);
             $statement->execute();
         } else {
-            $statement = $conn->prepare("select p.*, count(l.user_id) as likes, u.username as username, u.image as userImage from posts p inner join users u on p.userId = u.id left join likes l on p.id = l.post_id group by p.id order BY id DESC limit :noP,20");
+            $statement = $conn->prepare("select p.*, count(r.postId), count(l.user_id) as likes, u.username as username, u.image as userImage, p.userId, r.postId
+                                        from posts p
+                                        left join reported r on r.postId = p.id
+                                        inner join users u on p.userId = u.id 
+                                        left join likes l on p.id = l.post_id
+                                        group by p.id
+                                        HAVING count(r.postId) < 3
+                                        order BY id DESC limit :noP,20");
             $statement->bindValue(':noP', $noP, PDO::PARAM_INT);
             $statement->execute();
         }
